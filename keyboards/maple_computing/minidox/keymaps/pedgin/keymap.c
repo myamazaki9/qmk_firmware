eaf8b7c5647f741f47e0bc0382c7178f99297abb
myamazaki9 <myamazaki9@ixas.unisys.co.jp>
2025-08-13 13:55:56 +0900

    Fix typo in keymap definitions and update layer assignments

--- keyboards/maple_computing/minidox/keymaps/pedgin/keymap.c
+++ keyboards/maple_computing/minidox/keymaps/pedgin/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT(
          KC_EXLM,           KC_AT,         KC_HASH,          KC_DLR,         KC_PERC,             KC_CIRC,         KC_AMPR,         KC_ASTR,         KC_MINS,          KC_EQL,
-            KC_1,            KC_2,            KC_3,            KC_4,             KC5,                KC_6,            KC_7,            KC_8,            KC_9,            KC_0,
+            KC_1,            KC_2,            KC_3,            KC_4,            KC_5,                KC_6,            KC_7,            KC_8,            KC_9,            KC_0,
          _______,         _______,         _______,         _______,         _______,             _______,         _______,          KC_GRV,         KC_QUOT,         KC_BSLS,
                                            _______,         _______,         _______,             _______,         _______,         _______
 ),
@@ -123,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_TAB,            KC_Q,            KC_W,            KC_E,            KC_R,                KC_Y,            KC_U,            KC_I,            KC_O,            KC_P,
          KC_LCTL,            KC_A,            KC_S,            KC_D,            KC_F,                KC_H,            KC_J,            KC_K,            KC_L,            KC_N,
          KC_LSFT,            KC_Z,            KC_X,            KC_C,            KC_V,                KC_N,            KC_M,         KC_COMM,          KC_DOT,         KC_BSLS,
-                                           KC_LZLT,         _QWENUM,          KC_SPC,              KC_ENT,        _EUCALYN,         KC_LGUI
+                                           KC_LALT,         _QWENUM,          KC_SPC,              KC_ENT,        _EUCALYN,         KC_LGUI
 ),
 
 /* QWERTY NUM
@@ -142,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                |      |    |      |
  *                                `------'    `------'
  */
-[_QWERTY] = LAYOUT(
+[_QWENUM] = LAYOUT(
           KC_ESC,            KC_Q,            KC_W,            KC_E,            KC_R,             _______,         _______,         _______,         _______,         _______,
          _______,            KC_A,            KC_S,            KC_D,            KC_F,             _______,         _______,         _______,         _______,         _______,
          _______,            KC_Z,            KC_X,            KC_C,            KC_V,             _______,         _______,         _______,         _______,         _______,
